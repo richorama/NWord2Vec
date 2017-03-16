81bda612f8193be573223469ad9a08c4f67ec800
Richard Astbury <richard.astbury@gmail.com>
2017-03-16 13:25:17 +0000

    additonal attributes for nuspec

--- NWord2Vec/Properties/AssemblyInfo.cs
+++ NWord2Vec/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NWord2Vec")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# library for working with Word2Vec models")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Richard Astbury")]
 [assembly: AssemblyProduct("NWord2Vec")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.3.0")]
 [assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyInformationalVersion("1.0.3.0")]
